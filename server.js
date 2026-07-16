a5285d0ae81ea137d482fb93a24ccfb755e99092
kenil919 <kenil.developer199@gmail.com>
2026-07-16 09:50:19 +0530

    server is running

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const { createClient } = require("@supabase/supabase-js");
-
-dotenv.config();
+const WebSocket = require("ws");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-// Connect to Supabase
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_KEY,
+  {
+    realtime: {
+      transport: WebSocket
+    }
+  }
 );
 
 // Home route
